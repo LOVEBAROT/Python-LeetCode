90e51be7d7bc1c4928fcd19978407e83083a9ebd
LOVE R BAROT <35568343+LOVEBAROT@users.noreply.github.com>
2022-02-09 15:57:45 +0530

    Add files via upload

---
+++ Roman to Integer.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def romanToInt(self, s):
+        rd = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        n = len(s)
+        rt = 0
+        for i in range(n):
+            if i == n - 1 or rd[s[i]] >= rd[s[i + 1]]:
+                rt += rd[s[i]]
+            else:
+                rt -= rd[s[i]]
+
+        return rt
