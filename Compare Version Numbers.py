370ebe43ed603d04e49c14aeddb83ecf87477946
LOVE R BAROT <35568343+LOVEBAROT@users.noreply.github.com>
2022-02-25 22:56:54 +0530

    Add files via upload

---
+++ Compare Version Numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        version1_list = version1.split('.')
+        version2_list = version2.split('.')
+        
+        len1 = len(version1_list)
+        len2 = len(version2_list)
+        
+        for i in range(max(len1,len2)):
+            
+            i1 = int(version1_list[i]) if i < len1 else 0
+            
+            i2 = int(version2_list[i]) if i < len2 else 0
+            
+            if i1 != i2:
+                return 1 if i1 > i2 else -1
+        return 0
