dbfc4afe1754722510d7e16c850ad20f05c3dee7
LOVE R BAROT <35568343+LOVEBAROT@users.noreply.github.com>
2022-03-21 13:16:08 +0530

    Add files via upload

---
+++ IsAnagram.c
@@ -0,0 +1,13 @@
+ bool isAnagram(string a, string b){
+       
+       sort(a.begin(),a.end());
+       sort(b.begin(),b.end());
+       if(a==b)
+       {
+           return true;
+       }
+       else 
+       {
+           return false;
+       }
+   }
